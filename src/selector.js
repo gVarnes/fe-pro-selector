aec16f16edf87c44898ed03b7311ea18375b3e2b
Vladislav Kolomoiets <vellenger17@gmail.com>
2021-12-22 23:08:47 +0200

    changed name of parametr keys to path

--- src/selector.js
+++ src/selector.js
@@ -1,18 +1,18 @@
-const selector = (obj, keys) => {
+const selector = (obj, path) => {
     // Ваше решение должно быть здесь
     //Написать ф-цию которая идет по пути и возвращает значение, если такого пути нет вернуть пустую строку.
     //Всё что сейчас написано под сомнением.
     let result = obj;
     let test;
-    for (let i = 0; i < keys.length; i++) {
-        test = keys[i];
+    for (let i = 0; i < path.length; i++) {
+        test = path[i];
         if (typeof result[test] === 'undefined') {
             return ''
         }
 
-        if (Object.keys(result)[0] === keys[i]) {
+        if (Object.keys(result)[0] === path[i]) {
             // нужено реализовать здесь условие как выше,а именно присвоить резалт и возможно обновлять тест
-            if ((i === keys.length - 1) && typeof result[test] !== 'object') {
+            if ((i === path.length - 1) && typeof result[test] !== 'object') {
                 return result[test]
             }
             result = obj[test];
