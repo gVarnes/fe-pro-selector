fdb0b250416a6ee0ef3e29ec55b2ee95f48391a0
Vladislav Kolomoiets <vellenger17@gmail.com>
2021-12-23 18:16:15 +0200

    canceled last push because it doesnt work, also deleted a few rows

--- src/selector.js
+++ src/selector.js
@@ -1,14 +1,15 @@
 const selector = (obj, path) => {
+    // Ваше решение должно быть здесь
     let result = obj;
     let test;
-    path.forEach(item => {
-        test = item;
-        if (Object.values(result)[0] !== undefined && Object.keys(result)[0] === item) {
+    for (let i = 0; i < path.length; i++) {
+        test = path[i];
+        if (Object.values(result)[0] !== undefined && Object.keys(result)[0] === path[i]) {
             result = result[test];
         } else {
             return '';
         }
-    });
+    }
     return result;
 };
 
